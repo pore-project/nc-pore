1c60c80daa4028891201064fa25af767eaee6482
Pore Project <pore@mediaoffice-zimmer.de>
2026-08-22 14:16:55 +0200

    feat: expose Nextcloud infrastructure module

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -1,6 +1,11 @@
+pub mod nextcloud;
 pub mod session_repository;
 pub mod synchronization_work_store;
 
+pub use nextcloud::{
+    HttpWebDavTransport, NextcloudConnection, NextcloudConnectionConfig, NextcloudCredentials,
+    NextcloudProviderError, WebDavClient, WebDavEntry, WebDavTransport, WebDavTransportError,
+};
 pub use session_repository::{
     FileProductionSessionRepository, FileProductionSessionRepositoryError,
 };
