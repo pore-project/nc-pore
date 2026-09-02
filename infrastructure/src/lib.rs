7c9e26ba9171ca2fbc235f58951d5c3ffccbd9ba
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-02 19:28:19 +0200

    style: format Nextcloud exports

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -3,9 +3,9 @@ pub mod session_repository;
 pub mod synchronization_work_store;
 
 pub use nextcloud::{
-    HttpWebDavTransport, NextcloudArtifactTransfer, NextcloudArtifactUploader,
-    NextcloudConnection, NextcloudConnectionConfig, NextcloudCredentials, NextcloudProviderError,
-    WebDavClient, WebDavEntry, WebDavTransport, WebDavTransportError,
+    HttpWebDavTransport, NextcloudArtifactTransfer, NextcloudArtifactUploader, NextcloudConnection,
+    NextcloudConnectionConfig, NextcloudCredentials, NextcloudProviderError, WebDavClient,
+    WebDavEntry, WebDavTransport, WebDavTransportError,
 };
 pub use session_repository::{
     FileProductionSessionRepository, FileProductionSessionRepositoryError,
