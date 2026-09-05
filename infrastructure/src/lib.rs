c4ae7794a7b2d301ecde21ce09e285dd90c39c73
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 16:23:54 +0200

    refactor(infrastructure): re-export storage work store

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod nextcloud;
 pub mod session_repository;
-pub mod synchronization_work_store;
 
 pub use nextcloud::{
     HttpWebDavTransport, NextcloudArtifactTransfer, NextcloudConnection, NextcloudConnectionConfig,
@@ -10,4 +9,4 @@ pub use nextcloud::{
 pub use session_repository::{
     FileProductionSessionRepository, FileProductionSessionRepositoryError,
 };
-pub use synchronization_work_store::FilesystemSynchronizationWorkStore;
+pub use nc_pore_storage::FilesystemSynchronizationWorkStore;
