2d026d4131b58e88d9fbf24d0354e811b8be67b0
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 17:13:41 +0200

    feat(nextcloud): expose PoRe settings endpoints

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -9,5 +9,15 @@ return [
 			'url' => '/v1/recordings/finalized-artifact',
 			'verb' => 'POST',
 		],
+		[
+			'name' => 'Settings#getSettings',
+			'url' => '/v1/settings',
+			'verb' => 'GET',
+		],
+		[
+			'name' => 'Settings#setSettings',
+			'url' => '/v1/settings',
+			'verb' => 'PUT',
+		],
 	],
 ];
