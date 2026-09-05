a46dc144296d7da2ba104f678ec51169dcd4762c
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 17:56:17 +0200

    refactor(v1): remove obsolete Nextcloud infrastructure

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -1,11 +1,5 @@
-pub mod nextcloud;
 pub mod session_repository;
 
-pub use nextcloud::{
-    HttpWebDavTransport, NextcloudArtifactTransfer, NextcloudConnection, NextcloudConnectionConfig,
-    NextcloudCredentials, NextcloudProviderError, WebDavClient, WebDavEntry, WebDavTransport,
-    WebDavTransportError,
-};
 pub use session_repository::{
     FileProductionSessionRepository, FileProductionSessionRepositoryError,
 };
