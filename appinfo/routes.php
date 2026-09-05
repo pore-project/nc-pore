0ed95eddb2bd223c67218805d7585b504364c839
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 13:12:55 +0200

    feat: add browser artifact submission route

---
+++ appinfo/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'routes' => [
+		[
+			'name' => 'browser_recording_artifact#submit',
+			'url' => '/api/recordings/artifact',
+			'verb' => 'POST',
+		],
+	],
+];
