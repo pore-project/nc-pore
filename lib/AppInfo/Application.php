509249db60e4e71f7bf45560472e180e240cd069
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 16:04:54 +0200

    feat(nextcloud): load browser runtime transport adapter

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -25,10 +25,10 @@ class Application extends App implements IBootstrap {
 		Util::addInitScript(self::APP_ID, 'pore-browser-pcm-persistence');
 		Util::addInitScript(self::APP_ID, 'pore-browser-completion-job');
 		Util::addInitScript(self::APP_ID, 'pore-recording-controller');
-		Util::addInitScript(self::APP_ID, 'pore-browser-pcm-recorder');
 		Util::addInitScript(self::APP_ID, 'pore-talk-audio-connector');
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-state-bridge');
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-ui');
+		Util::addInitScript(self::APP_ID, 'pore-browser-runtime-transport');
 		Util::addInitScript(self::APP_ID, 'init');
 	}
 }
