b86394eb7533281fa007ebb312d51f2e2ffbf3e5
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-05 16:03:58 +0200

    feat(nextcloud): add runtime artifact OCS route

---
+++ appinfo/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'ocs' => [
+		[
+			'name' => 'RecordingTransport#submitFinalizedArtifact',
+			'url' => '/v1/recordings/finalized-artifact',
+			'verb' => 'POST',
+		],
+	],
+];
