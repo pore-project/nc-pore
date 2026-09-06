479c9e11b716f5df1fa152b4e9f0e24e881b6cdf
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-06 11:24:18 +0200

    feat(v1): load Talk recording UI mount adapter

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -29,6 +29,7 @@ class Application extends App implements IBootstrap {
 		Util::addInitScript(self::APP_ID, 'pore-talk-audio-connector');
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-state-bridge');
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-ui');
+		Util::addInitScript(self::APP_ID, 'pore-talk-recording-ui-mount');
 		Util::addInitScript(self::APP_ID, 'pore-browser-runtime-transport');
 		Util::addInitScript(self::APP_ID, 'init');
 	}
