58c0eaaa02cf96d5fa8804bcde572fc98ac114c0
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-06 14:38:54 +0200

    feat(v1): load Talk recording host adapter

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -31,6 +31,7 @@ class Application extends App implements IBootstrap {
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-ui');
 		Util::addInitScript(self::APP_ID, 'pore-talk-recording-ui-mount');
 		Util::addInitScript(self::APP_ID, 'pore-browser-runtime-transport');
+		Util::addInitScript(self::APP_ID, 'pore-talk-recording-host');
 		Util::addInitScript(self::APP_ID, 'init');
 	}
 }
