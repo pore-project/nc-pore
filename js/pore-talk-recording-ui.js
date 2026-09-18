23f956ae4cfc2eac7d3cf84bfa79bd29765de35c
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-18 22:57:04 +0200

    fix(ui): avoid SVG className baseVal dependency

--- js/pore-talk-recording-ui.js
+++ js/pore-talk-recording-ui.js
@@ -95,7 +95,7 @@
 		const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 		svg.setAttribute('viewBox', '0 0 24 24')
 		svg.setAttribute('aria-hidden', 'true')
-		svg.className.baseVal = 'pore-talk-recording__logo'
+		svg.setAttribute('class', 'pore-talk-recording__logo')
 		const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
 		path.setAttribute('fill', 'currentColor')
 		path.setAttribute('d', 'M12 .7a12 12 0 0 0-3.79 23.39c.6.11.82-.26.82-.58v-2.05c-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.33-1.76-1.33-1.76-1.09-.75.08-.74.08-.74 1.2.08 1.84 1.23 1.84 1.23 1.07 1.83 2.81 1.3 3.5.99.11-.78.42-1.3.76-1.6-2.67-.3-5.47-1.34-5.47-5.95 0-1.31.47-2.38 1.23-3.22-.12-.3-.53-1.52.12-3.18 0 0 1-.32 3.3 1.23a11.5 11.5 0 0 1 6-.01c2.29-1.55 3.29-1.23 3.29-1.23.65 1.66.24 2.88.12 3.18.77.84 1.23 1.91 1.23 3.22 0 4.62-2.81 5.64-5.49 5.94.43.37.81 1.1.81 2.22v3.29c0 .32.22.69.83.57A12 12 0 0 0 12 .7Z')
@@ -107,7 +107,7 @@
 		const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
 		svg.setAttribute('viewBox', '0 0 24 24')
 		svg.setAttribute('aria-hidden', 'true')
-		svg.className.baseVal = 'pore-talk-recording__chevron'
+		svg.setAttribute('class', 'pore-talk-recording__chevron')
 		const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
 		path.setAttribute('fill', 'currentColor')
 		path.setAttribute('d', 'M7.41 15.41 12 10.83l4.59 4.58L18 14l-6-6-6 6z')
