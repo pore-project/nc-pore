806a443a1cdc649cb2dbfab9c866e6dc48f70288
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-18 21:38:18 +0200

    fix(browser): add explicit AudioWorklet capture flush

--- js/pore-browser-pcm-worklet.js
+++ js/pore-browser-pcm-worklet.js
@@ -1,7 +1,19 @@
 class PoREPcmProcessor extends AudioWorkletProcessor {
+	constructor() {
+		super()
+		this.flushRequested = false
+		this.port.onmessage = event => {
+			if (event.data?.type === 'flush') this.flushRequested = true
+		}
+	}
+
 	process(inputs) {
 		const input = inputs[0]?.[0]
 		if (input?.length) this.port.postMessage(input.slice())
+		if (this.flushRequested) {
+			this.port.postMessage({ type: 'flush-complete' })
+			this.flushRequested = false
+		}
 		return true
 	}
 }
