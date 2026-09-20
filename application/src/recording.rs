f66c624ed6c8dc276c5d85246c0089a6b1368fef
Pore Project <pore@mediaoffice-zimmer.de>
2026-09-20 11:46:04 +0200

    test(application): establish expected recording participant in fixture

--- application/src/recording.rs
+++ application/src/recording.rs
@@ -232,6 +232,15 @@ mod tests {
         session
             .add_recording_by(&actor, Recording::new(recording_id.value()))
             .unwrap();
+        session
+            .begin_recording_by(&actor, &recording_id, [actor.clone()])
+            .unwrap();
+        session
+            .mark_recording_ready_by(&actor, &recording_id)
+            .unwrap();
+        session
+            .confirm_recording_opening_by(&actor, &recording_id)
+            .unwrap();
         repository.store(&session).unwrap();
 
         (repository, production_id, actor, recording_id)
